879d7710724f545aa972876e49bf23dfcf82b1cf
NagaKanparthy <nkanaparthyper@live.com>
2016-12-06 09:56:51 -0500

    Add project files.

---
+++ hfcServer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hfcServer.Startup))]
+namespace hfcServer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
